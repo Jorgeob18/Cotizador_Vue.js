067e215feba86a31b762bedaa4de2a424eeae82d
Jorgeob18 <jorgealbertoob18@gmail.com>
2023-05-11 14:24:58 -0600

    Eventos en Vue.js: @ y v-on

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,22 @@
   //     Header
   //   }
   // }
+  function handleChange(e) {
+    console.log(e.target.value);
+  }
 </script>
 
 <template>
   <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+
+    <div class="my-5">
+      <input 
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        @input="handleChange"
+      >
+    </div>
   </div>
   
 </template>
